2b23ab393da2d8a8050dc0f65088b487e274860b
Ahmad Hesham <ahmadhesham285@gmail.com>
2016-08-03 23:42:06 +0200

    Created a primary employee list home page structure

--- views/home.php
+++ views/home.php
@@ -1,9 +1,56 @@
-<!DOCTYPE html>
 <html>
-<head>
-	<title></title>
-</head>
-<body>
 
-</body>
+	<head>
+
+		<title>Home</title>
+
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+	</head>
+
+	<body>
+
+		<div class="container">
+
+			<h1>Employees info</h1>
+
+			<table class="table table-striped">
+				
+				<thead>
+					
+					<tr>
+						
+						<th>#</th>
+						<th>Name</th>
+						<th>Email</th>
+
+					</tr>
+
+				</thead>
+
+				<tbody>
+
+					<tr>
+						
+						<td>1</td>
+						<td>Name value</td>
+						<td>Email value</td>
+						<td><a href="#">view</a></td>
+						<td><a href="#">edit</a></td>
+						<td><a href="#">delete</a></td>
+
+					</tr>
+				
+				</tbody>
+
+			</table>
+			
+			<button type="button" name="add-employee" class="btn btn-danger">Add Employee</button>
+		
+		</div>
+
+	</body>
+
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
 </html>
