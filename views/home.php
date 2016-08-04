d98a36d69d8e416a50db85d1eb7ef77df0e9eacd
Ahmad Hesham <ahmadhesham285@gmail.com>
2016-08-04 15:49:03 +0200

    added the add employee modal form

--- views/home.php
+++ views/home.php
@@ -5,7 +5,8 @@
 		<title>Home</title>
 
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-
+		<link rel="stylesheet" href="../assets/node_modules/jquery-modal/jquery.modal.css" type="text/css" media="screen" />
+		
 	</head>
 
 	<body>
@@ -45,12 +46,51 @@
 
 			</table>
 			
-			<button type="button" name="add-employee" class="btn btn-danger">Add Employee</button>
+
+			<form id="create-user-form" class="modal" style="display: none">
+			  	
+				<div class="row"><h3>Add Employee</h3></div>
+
+			  	<div class="form-group">
+
+    				<label for="employee-name">Name</label>
+    				<input type="text" class="form-control" id="employee-name" placeholder="Name">
+
+  				</div>
+
+  				<div class="form-group">
+
+    				<label for="employee-email">Email</label>
+    				<input type="email" class="form-control" id="employee-email" placeholder="Email">
+
+  				</div>
+
+  				<div class="form-group">
+
+    				<label for="employee-address">Address</label>
+    				<input type="text" class="form-control" id="employee-address" placeholder="Address">
+
+  				</div>
+
+  				<div class="form-group">
+
+    				<label for="employee-picture">Image upload</label>
+				    <input type="file" id="employee-picture">
+				    
+  				</div>
+
+  				<a href="#" class="btn btn-info" rel="modal:close">Add</button>
+
+			</form>
+
+			<a href="#create-user-form" rel="modal:open" role="button" class="btn btn-danger">Add Employee</a>
 		
 		</div>
 
 	</body>
-
+	
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+	<script src="../assets/node_modules/jquery/dist/jquery.min.js" type="text/javascript" charset="utf-8"></script>
+	<script src="../assets/node_modules/jquery-modal/jquery.modal.min.js" type="text/javascript" charset="utf-8"></script>
 
 </html>
